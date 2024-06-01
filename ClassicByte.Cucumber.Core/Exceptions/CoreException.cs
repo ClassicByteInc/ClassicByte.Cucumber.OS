461a942ba83caf088692a28b842771df65b2b66f
Shimikoi <shimikoiku@outlook.com>
2024-06-01 20:32:01 +0800

    虽然哦分不清

--- ClassicByte.Cucumber.Core/Exceptions/CoreException.cs
+++ ClassicByte.Cucumber.Core/Exceptions/CoreException.cs
@@ -7,13 +7,25 @@ using System.Threading.Tasks;
 namespace ClassicByte.Cucumber.Core.Exceptions
 {
     /// <summary>
-    /// 
+    /// Cucumber中所有异常的基类，表示在Cucumber运行时发生的异常
     /// </summary>
     [Serializable]
     public class CoreException : Exception
     {
+        /// <summary>
+        /// 实例化一个异常
+        /// </summary>
         public CoreException() { }
+        /// <summary>
+        /// 实例化一个异常，并指定其消息
+        /// </summary>
+        /// <param name="message"></param>
         public CoreException(string message) : base(message) { }
+        /// <summary>
+        /// 实例化一个异常，指定其消息和内部异常
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
         public CoreException(string message, Exception inner) : base(message, inner) { }
         protected CoreException(
           System.Runtime.Serialization.SerializationInfo info,
--- ClassicByte.Cucumber.Core/Exceptions/Error.cs
+++ ClassicByte.Cucumber.Core/Exceptions/Error.cs
@@ -7,12 +7,31 @@ using System.Threading.Tasks;
 namespace ClassicByte.Cucumber.Core.Exceptions
 {
 
+    /// <summary>
+    /// Cucumber遇到的无法处理的错误类
+    /// </summary>
     [Serializable]
     public class Error : Exception
     {
+        /// <summary>
+        /// 实例化一个错误
+        /// </summary>
         public Error() { }
+
+        /// <summary>
+        /// 实例化一个错误，并指定消息和错误代码
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exitCode"></param>
         public Error(string message, String exitCode) : base(message) { ErrorCode = exitCode; }
-        public Error(string message, Exception inner) : base(message, inner) { }
+
+        /// <summary>
+        /// 实例化一个错误,并指定其消息
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exitCode"></param>
+        /// <param name="inner"></param>
+        public Error(string message, String exitCode, Exception inner) : base(message, inner) { }
         public String ErrorCode { get; set; }
         public void Print()
         {
