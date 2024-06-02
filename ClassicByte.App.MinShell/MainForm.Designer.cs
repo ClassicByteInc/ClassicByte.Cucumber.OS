ee2500ea9e9a9050d8c8c3c0a3e3fd7a7253ecfe
Shimikoi <shimikoiku@outlook.com>
2024-06-02 20:15:15 +0800

    迷你Shell

---
+++ ClassicByte.App.MinShell/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ClassicByte.App.MinShell
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.操作ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InputBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.操作ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(11, 4, 0, 4);
+            this.menuStrip1.Size = new System.Drawing.Size(978, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // 操作ToolStripMenuItem
+            // 
+            this.操作ToolStripMenuItem.Name = "操作ToolStripMenuItem";
+            this.操作ToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.操作ToolStripMenuItem.Text = "操作";
+            this.操作ToolStripMenuItem.Click += new System.EventHandler(this.操作ToolStripMenuItem_Click);
+            // 
+            // InputBox
+            // 
+            this.InputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InputBox.Location = new System.Drawing.Point(0, 36);
+            this.InputBox.Name = "InputBox";
+            this.InputBox.Size = new System.Drawing.Size(978, 564);
+            this.InputBox.TabIndex = 1;
+            this.InputBox.Text = "";
+            this.InputBox.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.InputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.InputBox_KeyDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 600);
+            this.Controls.Add(this.InputBox);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 操作ToolStripMenuItem;
+        internal System.Windows.Forms.RichTextBox InputBox;
+    }
+}
---
+++ ClassicByte.App.MinShell/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassicByte.App.MinShell
+{
+    public partial class MainForm : Form
+    {
+        public static StringBuilder Command { get; set; }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("wdnm","dw",MessageBoxButtons.OK,MessageBoxIcon.Hand);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        public String ReadLine()
+        {
+            InputBox.ReadOnly = false;
+            var command = new StringBuilder();
+            int time = 0;
+            while (true)
+            {
+
+                var theChar = InputBox.Text.ToCharArray()[time];
+                if ((theChar=='\n')||time== InputBox.Text.ToCharArray().Length)
+                {
+                    InputBox.ReadOnly = true;
+                    return command.ToString();
+                }
+                command.Append(theChar);
+                time++;
+            }
+        }
+
+        private void 操作ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(ReadLine());
+
+        }
+    }
+}
